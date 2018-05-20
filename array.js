66a3d7d6b5576e1549e8bef1d6a37756a59e2fd3
allexandrucrisan <allexandru.crisan@gmail.com>
2018-05-20 16:36:14 +0300

    added bracars

--- array.js
+++ array.js
@@ -1,5 +1,6 @@
 var numere = [2, 5, 7, 8, 13, 15, 74, 1346, 13572458, 1231435];
-for (1=0; numere.length<=10, i++)
+for (1=0; numere.length<=10, i++){
     if (numere%2 == 0){
         console.log(nume + "este numat par");
     }
+}
