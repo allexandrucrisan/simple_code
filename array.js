b5f919ec93fc7445ac3c400306721b4f1d4a4822
allexandrucrisan <35578000+allexandrucrisan@users.noreply.github.com>
2018-05-20 17:17:25 +0300

    Update array.js

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 var numere = [2, 5, 7, 8, 13, 15, 74, 1346, 13572458, 1231435];
 for (var i=0; i<numere.length; i++){
     if (numere[i]%2 == 0){
-        console.log(numere[i] + "este numat par");
+        console.log(numere[i] + " este numar par");
     }
 }
