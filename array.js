563f1e361cdb2b0ea0d99f72397461324afc637f
AdrianCiuciui <31804204+AdrianCiuciui@users.noreply.github.com>
2018-05-21 20:11:20 +0300

    One liner response

--- array.js
+++ array.js
@@ -1,6 +1,9 @@
+var numereNew = [];
 var numere = [2, 5, 7, 8, 13, 15, 74, 1346, 13572458, 1231435];
 for (var i=0; i<numere.length; i++){
     if (numere[i]%2 == 0){
-        console.log(numere[i] + " este numar par");
+        numereNew.push(numere[i]);
     }
 }
+var str = numereNew.join();
+console.log("numerele pare sunt: " + str);
